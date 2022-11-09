1d9216b9ba4c0b6d82401168cbe6f12a8c6a8bbf
fannizavecz <117857235+fannizavecz@users.noreply.github.com>
2022-11-09 19:43:31 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,56 @@
+let amitAKockaDobott = 0
+input.onGesture(Gesture.Shake, function () {
+    amitAKockaDobott = randint(1, 6)
+    if (amitAKockaDobott == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (amitAKockaDobott == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (amitAKockaDobott == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (amitAKockaDobott == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (amitAKockaDobott == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
